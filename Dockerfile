2e62cbf6f5d0dae32e4ac82caaff8e63250f29c4
Pim van der Loos <pimvanderloos@gmail.com>
2021-06-27 19:39:44 +0200

    Reduce Docker image size, auto-download
    - Reduced the size of the resulting docker image by about 50% by all the runs into a single layer and by giving up on compiling Python and just installing it from the repo.
    - The image will now be downloaded and installed if needed when running the script.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:20.04
-ARG DEBIAN_FRONTEND="noninteractive" 
+ARG DEBIAN_FRONTEND="noninteractive"
 ENV TZ="Europe/Amsterdam"
 COPY . /hwr
 WORKDIR /hwr
@@ -9,34 +9,12 @@ RUN \
  echo "***** Installing basic dependencies *****" && \
  apt-get update && \
  apt-get install -y \
-     libssl-dev openssl git wget build-essential
-
-# Compile and install python 3.8.7
-RUN \
- echo "***** Compiling Python 3.8.7 *****" && \
- echo "deb-src http://archive.ubuntu.com/ubuntu/ focal main" >> /etc/apt/sources.list && \
- apt-get update && \
- apt-get build-dep -y python3 && \
- apt-get install -y gdb lcov libbz2-dev libffi-dev \
-      libgdbm-dev liblzma-dev libncurses5-dev libreadline6-dev \
-      libsqlite3-dev libssl-dev lzma lzma-dev tk-dev uuid-dev zlib1g-dev && \
- wget https://www.python.org/ftp/python/3.8.7/Python-3.8.7.tgz && \
- tar -xzvf Python-3.8.7.tgz && \
- cd Python-3.8.7 && \
- ./configure --with-ensurepip=install && \
- make && \
- make install
-
-# Set up our project
-RUN \
+     libssl-dev openssl python3 python3-pip python3-distutils && \
  echo "***** Setting up project  *****" && \
  python3 -m pip install --upgrade pip && \
- python3 -m pip install -r requirements.txt
-
-# Cleanup
-RUN \
+ python3 -m pip install -r requirements.txt && \
  apt-get purge --auto-remove -y \
-        build-essential libssl-dev openssl git wget && \
+        libssl-dev openssl && \
  apt-get clean && \
  rm -rf \
         /tmp/* \
--- run.sh
+++ run.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+function download_image() {
+    echo "Image not found... Going to download it now, this may take a second!"
+    image_archive="save_tree_save_world_hwr.docker"
+    wget -q --show-progress -O "$image_archive" http://dl.pim16aap2.nl/s/SKmdkX4t7bi3G9Z/download/save_tree_save_world_hwr.docker
+    test $(stat --printf="%s" save_tree_save_world_hwr.docker) -gt 1000000 || { echo "Failed to download file! Please get it manually here: "; echo "https://drive.google.com/file/d/1ckqogv-WzmlrzskR_wF4V4e5vtIH5Oh2/view?usp=sharing"; echo "And then import it this way ''"; exit 1; }
+    echo "The image has been downloaded! Going to import it now..."
+    docker load < "$image_archive"
+    rm "$image_archive"
+}
+
 # Ensure an output directory is specified.
-test -z $1 && { echo "No input directory specified! Usage: \"./run.sh [/]path/to/images/\""; exit 1; }
+test -z $1 && { echo "No input directory specified! Usage: \"./run.sh [/]path/to/images/\""; exit 2; }
 
 # Make sure the container doesn't already exist (as we're deleting it afterwards).
-docker container inspect save_tree_save_world_hwr > /dev/null 2>&1 && { echo "You already have a container named \"save_tree_save_world_hwr\"! Aborting!"; exit 2; }
+docker container inspect save_tree_save_world_hwr > /dev/null 2>&1 && { echo "You already have a container named \"save_tree_save_world_hwr\"! Aborting!"; exit 3; }
 
-# Create the results dir.
-test -d results || { mkdir results || exit 3; }
+# Download the image if the user does not have it yet.
+docker images | grep save_tree_save_world_hwr || download_image
 
 # Run the pipeline.
 docker run --user $(id -u):$(id -g) --name save_tree_save_world_hwr -v "$(pwd)/results/":/output -v "$1":/input save_tree_save_world_hwr
