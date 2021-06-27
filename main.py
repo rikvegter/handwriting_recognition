37c52f6cb47c6dbedf2826b8bf43036f38493230
Pim van der Loos <pimvanderloos@gmail.com>
2021-06-27 12:51:15 +0200

    Listen to input args for output file
    - The transcribed text is now written to the specified output. If no output is specified, it'll be written to stdout.
    - Added the classifier/ngram paths as cli options.

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ from segmentation.character import CharacterSegmenter
 from segmentation.line import LineSegmenter
 from segmentation.options import SegmentationOptions
 from word_recognition.ngram_processor import NGramProcessor
+from word_recognition.options import ClassifierOptions
 from word_recognition.word_classifier import WordClassifier
 
 
 def main(args):
     general_options: GeneralOptions = args.general
     segment_options: SegmentationOptions = args.segmentation
+    classifier_options: ClassifierOptions = args.classifier
 
     # Segment lines
     line_segmenter = LineSegmenter(general_options=general_options,
@@ -40,20 +42,22 @@ def main(args):
         exit()
 
     # Classify characters
-    word_classifier = WordClassifier(character_classifier="character_recognition/data/classification_model/")
+    word_classifier = WordClassifier(character_classifier=classifier_options.classifier)
     # Reduce Line[Words[Char[]]] to Line[Char[]]
     segmented_image_lines: List[List[np.ndarray]] = [[char for word in line for char in word] for line in segmented_image]
     classified_lines: List[List[int]] = word_classifier.classify_words(segmented_image_lines, True)
 
     # Use bi-grams to improve accuracy
-    ngp = NGramProcessor("util/ngrams_frequencies_withNames.xlsx.txt", ngram_length=2)
+    ngp = NGramProcessor(classifier_options.ngram_file, ngram_length=2)
     classified_lines: List[List[int]] = ngp.predict_multiple(classified_lines)
 
     # Get the final transcribed lines as strings of unicode characters
-    transcribed_lines: List[str] = utils.decode_words(classified_lines)
-
-    print("transcribed_lines: \n=========================\n{}\n=========================\n"
-          .format("\n".join(transcribed_lines)))
+    transcribed_output = "\n".join(utils.decode_words(classified_lines))
+    if general_options.output_path is not None:
+        with open(general_options.output_path, encoding="utf-8", mode="w+") as file:
+            file.write(transcribed_output)
+    else:
+        print(transcribed_output)
 
     # Classify style
     # TODO
@@ -72,6 +76,9 @@ if __name__ == "__main__":
 
     # Add segmentation options
     parser.add_arguments(SegmentationOptions, dest="segmentation")
+
+    # Add classification options
+    parser.add_arguments(ClassifierOptions, dest="classifier")
 
     args = parser.parse_args()
 
--- options.py
+++ options.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Optional
+
 from simple_parsing import ArgumentParser, field
 from simple_parsing.helpers.fields import choice, flag
 
@@ -10,16 +12,16 @@ class GeneralOptions:
     input_path: str = field(alias="-i")
 
     # The path to save output to
-    output_path: str = field(default="./", alias="-o")
+    output_path: Optional[str] = field(default=None, alias="-o")
 
     # Whether to stop the process after a given step. Useful for debugging.
-    
+
     # [Step 1]: Line segmentation
     # [Step 2]: Character segmentation
     # [Step 3]: Character recognition
     # [Step 4]: Style classification (final step)
     stop_after: int = choice(1, 2, 3, 4, default=4,action="store",type=int)
-    
+
 
     # Save the results of intermediate steps to a debug directory in the output
     # path
