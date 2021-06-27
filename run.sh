2f57bf08ea3b7bcf6e9814d22fbe0d67e14c5583
Pim van der Loos <pimvanderloos@gmail.com>
2021-06-27 17:17:11 +0200

    Clean up container after execution

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+docker container inspect save_tree_save_world_hwr > /dev/null 2>&1 && { echo "You already have a container named \"save_tree_save_world_hwr\"! Aborting!"; exit 1; }
+
 cmd="python3 main.py $@"
-docker run --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined -v $(pwd):/files save_tree_save_world_hwr bash -c "bash /mergerfs.sh && cd /hwr_run && $cmd"
+
+docker run --cap-add SYS_ADMIN --name save_tree_save_world_hwr --device /dev/fuse --security-opt apparmor:unconfined -v $(pwd):/files save_tree_save_world_hwr bash -c "bash /mergerfs.sh && cd /hwr_run && $cmd"
+
+docker rm save_tree_save_world_hwr
