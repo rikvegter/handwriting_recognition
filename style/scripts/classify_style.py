aabeff0638c512ea07f8b8137a347025e83da87c
Pim van der Loos <pimvanderloos@gmail.com>
2021-06-28 12:43:46 +0200

    Use provided results path

--- style/scripts/classify_style.py
+++ style/scripts/classify_style.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     parser.add_argument('classifier_dir', type=check_dir, help='Work directory containing the saved classifier')
     parser.add_argument('results_dir', type=make_dir, help='Directory for writing classification results')
     args = parser.parse_args()
-    
+
     # Load classifier
     classifier_path = os.path.join(args.classifier_dir, 'classifier.joblib')
     classifier = joblib.load(classifier_path)
-    
+
     # Preprocess fragments
     preprocessing_args = classifier.preprocessing_args
     preprocessing_args.image_dir = args.image_dir
@@ -27,26 +27,26 @@ if __name__ == '__main__':
     preprocessing_args.labeled = False
     preprocessing_args.pattern = "*.jpg"
     preprocessing_args.name = "style_classification"
-    
+
     image_data = get_preprocessed_images_dataset(preprocessing_args)
     fraglet_extraction_args = classifier.fraglet_extraction_args
     fraglet_extraction_args.augment_times = 0
     classification_fraglet_data = get_fraglet_dataset(image_data, fraglet_extraction_args)
     dataset_path = os.path.join(args.classifier_dir, 'fraglets.nc')
-    
+
     print('Writing output to {}'.format(dataset_path))
     classification_fraglet_data.to_netcdf(dataset_path, encoding = {
         "contour": {"zlib" : True},
     })
-    
+
     normalize_fraglets(classification_fraglet_data)
     fragment_density = classifier.get_fragment_density(classification_fraglet_data)
-    result_path = make_dir("./results")
+    result_path = make_dir(args.results_dir)
     for img_id, frag_dict in fragment_density.items():
         r = classifier.classify_fragment(frag_dict)
         path = image_data.where(image_data.img_id == img_id, drop=True).img_path.item()
         basename = os.path.splitext(os.path.basename(path))[0]
-        style_path = os.path.join('./results', basename + "_style.txt")
+        style_path = os.path.join(args.results_dir, basename + "_style.txt")
         #style_path = ".".join(path.split('.')[:-1]) + "_style.txt"
         print(style_path)
         with open(style_path, 'w') as style_out_file:
@@ -55,8 +55,8 @@ if __name__ == '__main__':
 #     # Load fragments
 #     fragment_fraglets = read_fraglets(args.fragment_dir, classifier.args)
 #     normalize_fraglets(fragment_fraglets)
-                        
+
 #     fragment_density = classifier.get_fragment_density(fragment_fraglets)
-                        
+
 #     for img_id, frag_dict in fragment_density.items():
 #         r = classifier.classify_fragment(frag_dict)
